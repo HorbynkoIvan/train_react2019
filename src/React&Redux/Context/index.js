d528364dbd882c08579e9d11e088f09aae6efed1
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-03 22:43:11 +0300

    refactor, prepared for work

---
+++ src/React&Redux/Context/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Lesson extends Component{
+    render(){
+        return(
+            <div>!!!</div>
+        )
+    }
+}
+
+export default Lesson;
