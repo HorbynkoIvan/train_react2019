8c8ec3e648317c44f27cf18bf67dd78c4fec781a
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-04 23:08:55 +0300

    connected eslint to project

--- src/React&Redux/Context/index.js
+++ src/React&Redux/Context/index.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import LevelOne from './LevelOne'
 
-export const GreetContext = React.createContext();
+export const GreetContext = React.createContext()
 
 class Lesson extends Component {
-    render() {
-        return (
-            <GreetContext.Provider value='Hello1'>
-                <LevelOne greet="Hello"/>
-            </GreetContext.Provider>
-        )
-    }
+  render () {
+    return (
+      <GreetContext.Provider value='Hello1'>
+        <LevelOne greet="Hello"/>
+      </GreetContext.Provider>
+    )
+  }
 }
 
-export default Lesson;
+export default Lesson
