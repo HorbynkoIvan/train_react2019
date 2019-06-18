6cdc1dc01e5c4cc088d0f9d1988b72fc720b4374
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-18 22:52:50 +0300

    installed react-collapse, and connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Lesson from './React&Redux/Context'
+import Collapse from './react-collapse'
 
 function App () {
   return (
     <div>
       <Lesson/>
+      <Collapse/>
     </div>
   )
 }
---
+++ src/react-collapse/Collapse.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Collapse} from 'react-collapse';
+
+const CollapseComponent = () => {
+  return (
+        <>
+            Collapse
+        </>
+  )
+}
+
+export default CollapseComponent
