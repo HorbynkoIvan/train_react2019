f6ddaae8aeb22494d6db7e32e6e69c9869723c06
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-18 23:03:03 +0300

    added useState and implement functionality

--- src/react-collapse/Collapse.js
+++ src/react-collapse/Collapse.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
-import {Collapse} from 'react-collapse';
+import React, { useState } from 'react'
+import { Collapse } from 'react-collapse'
 
 const CollapseComponent = () => {
+  const [isOpen, setOpen] = useState(false)
   return (
-        <>
-            Collapse
-        </>
+    <div>
+      <div onClick={() => setOpen(!isOpen)}>Click me please!</div>
+      <Collapse
+        isOpened={isOpen}
+      >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at beatae corporis dignissimos
+                distinctio eligendi eum illum, incidunt ipsa iure laudantium libero maiores minima minus necessitatibus
+                nisi nobis nulla odio odit praesentium quaerat quas quia quos soluta suscipit velit voluptatem
+                voluptates. Ab adipisci enim eum explicabo ipsam, qui repudiandae tempore.
+      </Collapse>
+    </div>
   )
 }
 
