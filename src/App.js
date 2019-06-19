f8875aa5669ab4f43fd0c6339dd66a5120f14440
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-19 23:25:53 +0300

    adding arrow-btn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Lesson from './React&Redux/Context'
 import Collapse from './react-collapse'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 1366px;
+  width: 100%;
+  margin: 0 auto;
+`
 
 function App () {
   return (
-    <div>
+    <Container>
       <Lesson/>
       <Collapse/>
-    </div>
+    </Container>
   )
 }
 
--- src/react-collapse/Collapse.js
+++ src/react-collapse/Collapse.js
@@ -1,20 +1,71 @@
 import React, { useState } from 'react'
 import { Collapse } from 'react-collapse'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 700px;
+`
+
+const ClickMe = styled.div`
+  position:relative;
+  background: chocolate;
+  padding: 30px;
+  .arrow-btn{
+    position: absolute;
+    display: inline-block;
+    vertical-align: middle;
+    width: 20px;
+    height: 20px;
+    transition: background-color .3s;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 17px;
+     &:before,
+     &:after {
+       content: '';
+       display: block;
+       width: 8px;
+       height: 2px;
+       background: #7779ce;
+       transition: background-color .3s;
+          }
+     &:before {
+       position: absolute;
+       top: 50%;
+       right: 32%;
+       transform: rotate(45deg);
+        }
+     &:after {
+       position: absolute;
+       top: 50%;
+       left: 50%;
+       transform: rotate(-45deg);
+      }
+  }
+`
+
+const StyledCollapse = styled(Collapse)`
+  background: tomato;
+  .ReactCollapse--content{
+    padding: 30px 40px;
+  }
+`
 
 const CollapseComponent = () => {
   const [isOpen, setOpen] = useState(false)
   return (
-    <div>
-      <div onClick={() => setOpen(!isOpen)}>Click me please!</div>
-      <Collapse
+    <Container>
+      <ClickMe onClick={() => setOpen(!isOpen)}>Click me please!<span className="arrow-btn"></span></ClickMe>
+      <StyledCollapse
         isOpened={isOpen}
       >
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at beatae corporis dignissimos
                 distinctio eligendi eum illum, incidunt ipsa iure laudantium libero maiores minima minus necessitatibus
                 nisi nobis nulla odio odit praesentium quaerat quas quia quos soluta suscipit velit voluptatem
                 voluptates. Ab adipisci enim eum explicabo ipsam, qui repudiandae tempore.
-      </Collapse>
-    </div>
+      </StyledCollapse>
+    </Container>
   )
 }
 
