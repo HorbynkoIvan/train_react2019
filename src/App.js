5dd91747acd4d2ae87d63a874fb4a7710ee45a9b
HorbynkoIvan <horbynko.i@gmail.com>
2020-03-19 20:39:08 +0200

    added pretier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import Lesson from './React&Redux/Context'
-import Collapse from './react-collapse/Collapse'
-import Footer from './components/footer/Footer'
-import InlineSVGSComponent from './react_inline_svg/inline_svgs'
-import styled from 'styled-components'
+import React from "react";
+import Lesson from "./React&Redux/Context";
+import Collapse from "./react-collapse/Collapse";
+import Footer from "./components/footer/Footer";
+import InlineSVGSComponent from "./react_inline_svg/inline_svgs";
+import styled from "styled-components";
 
 const Container = styled.div`
   max-width: 1366px;
   width: 100%;
   margin: 0 auto;
-`
+`;
 
-function App () {
+function App() {
   return (
-    <Container>
-      <Lesson/>
-      <Collapse/>
-      <InlineSVGSComponent/>
-      <Footer/>
-    </Container>
-  )
+    <div>
+      <Container>
+        <Lesson />
+        <Collapse />
+        <InlineSVGSComponent />
+      </Container>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
